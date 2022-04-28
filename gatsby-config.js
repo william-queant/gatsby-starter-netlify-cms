5a4017e69f7daa56bf166adb100220ccfe390978
william-queant <william.queant@xero.com>
2022-04-28 14:59:07 +1200

    Fix dependencies errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "Gatsby + Netlify CMS Starter",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
   },
   plugins: [
     "gatsby-plugin-react-helmet",
@@ -10,31 +10,31 @@ module.exports = {
       resolve: "gatsby-plugin-sass",
       options: {
         sassOptions: {
-          indentedSyntax: true,
-        },
-      },
+          indentedSyntax: true
+        }
+      }
     },
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: "uploads",
-      },
+        name: "uploads"
+      }
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: "pages",
-      },
+        name: "pages"
+      }
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: "images",
-      },
+        name: "images"
+      }
     },
     `gatsby-plugin-image`,
     "gatsby-plugin-sharp",
@@ -44,10 +44,10 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: "gatsby-remark-relative-images",
+            resolve: "gatsby-remark-relative-images-v2",
             options: {
-              name: "uploads",
-            },
+              name: "uploads"
+            }
           },
           {
             resolve: "gatsby-remark-images",
@@ -55,31 +55,31 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
+              maxWidth: 2048
+            }
           },
           {
             resolve: "gatsby-remark-copy-linked-files",
             options: {
-              destinationDir: "static",
-            },
-          },
-        ],
-      },
+              destinationDir: "static"
+            }
+          }
+        ]
+      }
     },
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
     },
     {
       resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
-      },
+        purgeOnly: ["/all.sass"] // applies purging only on the bulma css file
+      }
     }, // must be after other CSS plugins
-    "gatsby-plugin-netlify", // make sure to keep it last in the array
-  ],
+    "gatsby-plugin-netlify" // make sure to keep it last in the array
+  ]
 };

c3e214363f8fefb4514b1ba1c0bdf06bcb7c113f
william-queant <william.queant@xero.com>
2022-04-29 11:37:16 +1200

    Revert "Set default image format to webp"
    This reverts commit a97e005bb38c5786f13edfabb348c5d1039ff322.

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,13 +43,13 @@ module.exports = {
         // Defaults used for gatsbyImageData and StaticImage
         defaults: {},
         // Set to false to allow builds to continue on image errors
-        failOnError: false,
+        failOnError: true,
         // deprecated options and their defaults:
         base64Width: 20,
-        forceBase64Format: `webp`, // valid formats: png,jpg,webp
+        forceBase64Format: ``, // valid formats: png,jpg,webp
         useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
         stripMetadata: true,
-        defaultQuality: 80
+        defaultQuality: 50
       }
     },
     "gatsby-transformer-sharp",
